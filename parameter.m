ca4d5c4b1f7ea3b4e980250dc26fb38f76fe9004
shafaartamevia <saafartm@gmail.com>
2025-06-25 17:19:13 +0700

    finishing no 1 + lanjut no 2

--- parameter.m
+++ parameter.m
@@ -1,3 +1,7 @@
+%% ==================================
+% deklarasi variabel yang diasumsikan
+%% ==================================
+
 Vin = 17.5
 Vo = 7;
 fsw = 10000;
@@ -14,23 +18,22 @@ L = ((1-D) * Vo) / (di * fsw) ;
 C = (Vo * (1 - D)) / (8*L*dv*(fsw*fsw));
 
 %% =====================
-% OUTER LOOP (Voltage)
+% parameter outer loop
 %% =====================
 
-outer_Fbp = fsw / 20;              % Bandwidth Proportional Voltage (500 Hz)
-outer_Fbi = outer_Fbp / 10;        % Bandwidth Integral Voltage (50 Hz)
+outer_Fbp = fsw / 20;              
+outer_Fbi = outer_Fbp / 10;        
 
 outer_Kp  = 2 * pi * outer_Fbp * C;           % Kp_v = 2πfC
 outer_Ki  = 2 * pi * outer_Fbi * outer_Kp;    % Ki_v = 2πf * Kp
 
 %% =====================
-% INNER LOOP (Current)
+% parameter inner loop
 %% =====================
 
-inner_Fbp = fsw / 200;              % Bandwidth Proportional Current (1 kHz)
-inner_Fbi = inner_Fbp / 10;        % Bandwidth Integral Current (100 Hz)
+inner_Fbp = fsw / 200;              
+inner_Fbi = inner_Fbp / 10;       
 inner_Fbd = inner_Fbp * 10;  
 
 inner_Kp  = 2 * pi * inner_Fbp * L;           % Kp_i = 2πfL
 inner_Ki  = 2 * pi * inner_Fbi * inner_Kp;    % Ki_i = 2πf * Kp
-inner_Kd  = 1 / (2 * pi * inner_Fbd * L);     % Kd_i ≈ 1 / (2πfL)
